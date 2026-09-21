feb59c01474a1386bb9d67bb0d8da8cce2b0c391
Duy-Phan-prg <phanduy.prg@gmail.com>
2026-09-21 19:52:27 +0700

    first commit

---
+++ JavaBasic/src/lesson01_fundamentals/HelloWorld.java
@@ -0,0 +1,9 @@
+package lesson01_fundamentals;
+//Package = thư mục phân loại bài học
+
+public class HelloWorld {                                       //← CLASS
+    public static void main(String[] args) {                    //← METHOD
+        System.out.println("Hello World");                      //STATEMENT
+    }
+}
+ 
